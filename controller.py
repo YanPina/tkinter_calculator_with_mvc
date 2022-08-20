c35bc1fe0b149324527a2e2c72d4f24a0005d9e1
YanPina <yanpina85@gmail.com>
2022-08-19 23:16:09 -0300

    controller

---
+++ controller.py
@@ -0,0 +1,10 @@
+class Controller:
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("In main of controller")
+
+if __name__ == '__main__':
+    calculator = Controller()
+    calculator.main()
