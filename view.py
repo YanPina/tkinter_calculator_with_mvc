781a9c8a5bd9077048a0d558188c60cd5d22dfe1
YanPina <yanpina85@gmail.com>
2022-08-20 21:51:14 -0300

    Finished styling

--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+from sre_parse import expand_template
 import tkinter as tk
 from tkinter import ttk
 
@@ -31,7 +32,7 @@ class View(tk.Tk):
 
         self.config(bg='black')
 
-        self.configure_button_styles()
+        self._configure_button_styles()
 
 
         self._make_main_frame()
@@ -40,11 +41,20 @@ class View(tk.Tk):
         self._center_window()
 
 
-    def configure_button_styles(self):
+    def _configure_button_styles(self):
         style = ttk.Style()
 
         style.theme_use('alt')
 
+        #Style for number buttons
+        style.configure('N.TButton', foreground='white', background='gray')
+
+        #Style for operators buttons
+        style.configure('O.TButton', foreground='white', background='orange')
+
+
+        #Style for miscellaneous buttons
+        style.configure('M.TButton', background='white')
 
     def main(self):
         self.mainloop()
@@ -63,26 +73,51 @@ class View(tk.Tk):
         outer_frm = ttk.Frame(self.main_frame)
         outer_frm.pack()
 
-        frm = ttk.Frame(outer_frm)
-        frm.pack()
+        is_first_row = True
 
         buttons_in_row = 0
 
         for caption in self.button_captions:
-            if buttons_in_row == self.MAX_BUTTONS_PER_ROW:
+            if is_first_row or buttons_in_row == self.MAX_BUTTONS_PER_ROW:
+                is_first_row = False
+
                 frm = ttk.Frame(outer_frm)
-                frm.pack()
+                frm.pack(fill='x')
 
                 buttons_in_row = 0
+
+            if isinstance(caption, int):
+                style_prefix = 'N'
+            elif self._is_operator(caption):
+                style_prefix = 'O'
+            else:
+                style_prefix = 'M'
+        
+
+            style_name = f'{style_prefix}.TButton'
+
 
             btn = ttk.Button(
                 frm, text=caption, command=(
                     lambda button=caption: self.controller.on_button_click(button)
-                )
+                ),
+                style = style_name
             )
-            btn.pack(side='left')
+
+            if caption == 0:
+                fill = 'x'
+                expand = 1
+            else:
+                fill = 'none'
+                expand = 0
+
+            btn.pack(fill=fill, expand=expand, side='left')
 
             buttons_in_row += 1
+
+
+    def _is_operator(self, button_caption):
+        return button_caption in ['/', '*', '-', '+', '=']
 
     def _center_window(self):
         self.update()
